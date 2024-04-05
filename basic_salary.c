cb720f6ac8e1e6b410b4a1a503f39ad4cb9beb51
sachingithuber <166120393+sachingithuber@users.noreply.github.com>
2024-04-05 15:28:12 +0530

    Add files via upload

---
+++ basic_salary.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+float calculate_gross_salary(float basic_salary) {
+float ta = 0.10 * basic_salary;
+float da = 0.12 * basic_salary;
+float gross_salary = basic_salary + ta + da;
+return gross_salary;
+}
+int main() {
+float basic_salary, gross_salary;
+printf("Enter the basic salary: ");
+scanf("%f", &basic_salary);
+gross_salary = calculate_gross_salary(basic_salary);
+printf("Gross Salary: %.2f\n", gross_salary);
+return 0;
+}
